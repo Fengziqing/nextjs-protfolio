265da16b268fc6ed7221cf42d72c094b7cb79f30
FENG <982685807@qq.com>
2024-05-12 18:50:26 +0800

    add default demo block style

--- app/bentoComponents/DemoBlock.tsx
+++ app/bentoComponents/DemoBlock.tsx
@@ -20,11 +20,15 @@ const DemoBlock = ({ imgsrc, imgalt, title, bgcolor, description }: Props) => {
         <button className={`btn w-96 h-44 rounded-3xl ${bgcolor === '' ? 'bg-[#F0EAE0]' : bgcolor} overflow-hidden p-0
                              hover:shadow-2xl`}
             onClick={handleClick}>
-            <Image src={imgsrc === '' ? imgsrc : '/images/todolist.png'} alt={imgalt} width={200} height={200} 
-                    className=' w-1/2 h-5/6 object-cover rounded-3xl border-solid border-gray-500' />
-            <div className='w-2/5'>
-                <p className=' text-lg font-bold text-left mb-3'>{title === '' ? 'developing block' : title}</p>
-                <div className=' text-sm font-normal text-left'>{description === '' ? 'place holder for testing, nothing here, if you have any ideas, please contact me!' : description}</div>
+            <div className={`${imgsrc === '' ? 'w-1/5' : 'w-1/2'}
+                                h-5/6 rounded-3xl border-2 border-solid border-gray-400 overflow-hidden`} >
+                <Image src={imgsrc === '' ? imgsrc : '/images/todolist.png'} alt={imgalt} width={200} height={200} 
+                        className=' h-full object-cover'
+                        onError={({currentTarget}) => currentTarget.style.display = 'none'}/>
+            </div>
+            <div className={`${imgsrc === '' ? 'w-3/5' : 'w-2/5'} pl-2`}>
+                <p className=' text-lg font-bold text-left mb-1'>{title === '' ? 'developing block' : title}</p>
+                <div className=' text-sm font-normal text-left'>{description === '' ? 'place-holder for testing, nothing here, if you have any ideas, please contact me!' : description}</div>
             </div>
         </button>
     )
--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -5,21 +5,26 @@ import DemoBlock from '../bentoComponents/DemoBlock'
 const Demo = () => {
   return (
     <div className="block">
-      <div className="grid md:grid-cols-row-col-8 md:grid-rows-row-col-8 gap-3 [&>*]:m-3">
+      <div className="grid md:grid-cols-row-col-8 md:grid-rows-row-col-8 gap-3 [&>*]:m-3
+                      grid-cols-col-4">
         <BentoTitle content='React Practice Demo' />
         <div className=" col-start-1 col-end-5 row-start-2 row-end-4">
           <DemoBlock imgsrc='ToDoList' imgalt='ToDoList' title='Todo-List' bgcolor='bg-[#9fb0cf]' description='React - useState Hook, HTML, CSS, Responsible Web Development' />
         </div>
-        <div className=" col-start-5 col-end-9 row-start-2 row-end-4">
+        <div className=" md:col-start-5 md:col-end-9 md:row-start-2 md:row-end-4
+                         col-start-1 col-end-5 row-start-4 row-end-6">
           <DemoBlock imgsrc='' imgalt='' title='' bgcolor='' description=''/>
         </div>
-        <div className=" col-start-1 col-end-5 row-start-4 row-end-6" >
+        <div className=" col-start-1 col-end-5 md:row-start-4 md:row-end-6
+                         row-start-6 row-end-8" >
           <DemoBlock imgsrc='' imgalt='' title='' bgcolor='' description=''/>
         </div>
-        <div className=" col-start-5 col-end-9 row-start-4 row-end-6">
+        <div className=" md:col-start-5 md:col-end-9 md:row-start-4 md:row-end-6
+                         col-start-1 col-end-5 row-start-8 row-end-10">
           <DemoBlock imgsrc='' imgalt='' title='' bgcolor='' description=''/>
         </div>
-        <div className=" col-start-1 col-end-5 row-start-6 row-end-8">
+        <div className=" col-start-1 col-end-5 md:row-start-6 md:row-end-8
+                         row-start-10 row-end-12">
           <DemoBlock imgsrc='' imgalt='' title='' bgcolor='' description=''/>
         </div>
       </div>
